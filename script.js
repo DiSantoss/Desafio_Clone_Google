82df455980f6920dbd6d3c7c7edc78e74e7bea6d
Diego <d.2120leandro@gmail.com>
2023-02-13 22:40:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+ function clicou(){
+    const menuicons = document.querySelector('#menuicons');
+
+    if (menuicons.style.display === "none") {
+        menuicons.style.display = "flex";
+    } else if (menuicons.style.display = "flex") {
+        menuicons.style.display = "none"
+    }
+
+
+ }
+
+
+
+ 
